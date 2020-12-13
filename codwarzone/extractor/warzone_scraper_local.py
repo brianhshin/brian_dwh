51fbfab6288865c526adce64a479989c026a85f7
brian shin <brianshin@brians-MacBook-Pro-2.local>
2020-12-13 13:30:13 -0500

    env var fix

--- codwarzone/extractor/warzone_scraper_local.py
+++ codwarzone/extractor/warzone_scraper_local.py
@@ -54,8 +54,8 @@ s3_bucket = 'codwarzone'
 
 s3 = boto3.client(
     's3',
-    aws_access_key_id=os.environ('aws_access_key_id'),
-    aws_secret_access_key=os.environ('aws_secret_access_key'))
+    aws_access_key_id=os.environ['aws_access_key_id'],
+    aws_secret_access_key=os.environ['aws_secret_access_key'])
 
 ################################################################################
 # for loading dataframes to s3 bucket
